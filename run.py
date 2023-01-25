c9e0330e040f8bf733298967e3177ba2f9d7ed3e
Kristina Orlichenko <chrisorlichenko@gmail.com>
2023-01-25 19:42:42 +0000

    Add words.py file with words for game

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Project 3 for Code Istitute
+"""
+import random
+import os
+from words import words_list
+from time import sleep
+ 
