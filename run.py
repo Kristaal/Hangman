29c257af42349424a74d5f88f55ee5c7cb57b92f
Kristina Orlichenko <chrisorlichenko@gmail.com>
2023-01-25 19:57:00 +0000

    add impors for selecting a random word, for clearing terminal, for using coloured text, for connect with words.py file and add variable statement

--- run.py
+++ run.py
@@ -3,6 +3,11 @@ Project 3 for Code Istitute
 """
 import random
 import os
-from words import words_list
-from time import sleep
+import words
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+MOVIES_LEVEL = words.movies_words
+CARS_LEVEL = words.cars_words
+ANIMALS_LEVEL = words.animals_words
  
